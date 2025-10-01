21f8efe7dd418905ba4a6e314c06b2a8e56d9f8c
BrainRotMaster <vanyastar660@gmail.com>
2025-10-01 11:18:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -66,24 +66,37 @@ line_pts = []             # 2 точки для линий
 tri_pts = []             # 3 точки для треугольника
 
 # -------------------- Рисование линий --------------------
-def bresenham_line(surf, x0, y0, x1, y1, color):
-    dx = abs(x1-x0)
-    dy = abs(y1-y0)
-    sx = 1 if x0 < x1 else -1
-    sy = 1 if y0 < y1 else -1
-    err = dx - dy
-    while True:
-        if in_canvas(x0, y0):
-            set_px(surf, x0, y0, color)
-        if x0 == x1 and y0 == y1:
-            break
-        e2 = 2*err
-        if e2 > -dy:
-            err -= dy
-            x0 += sx
-        if e2 < dx:
-            err += dx
-            y0 += sy
+def bresenham_line(surface, x0, y0, x1, y1, color):
+    dx = abs(x1 - x0)
+    dy = abs(y1 - y0)
+
+    sx = 1 if x1 > x0 else -1
+    sy = 1 if y1 > y0 else -1
+
+    if dy <= dx:
+        d = 2 * dy - dx
+        y = y0
+
+        for x in range(x0, x1 + sx, sx):
+            surface.set_at((x, y), color)
+
+            if d >= 0:  # if di ≥ 0 then yi+1 = yi + 1
+                y += sy
+                d += 2 * (dy - dx)  # di+1 = di + 2(dy - dx)
+            else:  # if di < 0 then yi+1 = yi
+                d += 2 * dy  # di+1 = di + 2dy
+    else:
+        d = 2 * dx - dy
+        x = x0
+
+        for y in range(y0, y1 + sy, sy):
+            surface.set_at((x, y), color)
+
+            if d >= 0:  # if di ≥ 0 then xi+1 = xi + 1
+                x += sx
+                d += 2 * (dx - dy)  # di+1 = di + 2(dx - dy)
+            else:  # if di < 0 then xi+1 = xi
+                d += 2 * dx  # di+1 = di + 2dx
 
 def wu_line(surf, x0, y0, x1, y1, color):
     def ipart(x):
